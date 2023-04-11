8db826f4cf6106755f2c571a2ed80f696e8a36ed
Osama-14 <mohdosama42000@gmail.com>
2023-04-11 17:31:05 +0500

    completed

--- app.js
+++ app.js
@@ -46,9 +46,69 @@
 //TASK : 4
 
 
-function addingList() {
-    const myList = document.getElementById('myList');
-    const newListItem = document.createElement('li');
-    newListItem.textContent = 'apples.';
-    myList.appendChild(newListItem);
-  }
+// function addingList() {
+//     const myList = document.getElementById('myList');
+//     const newListItem = document.createElement('li');
+//     newListItem.textContent = 'apples.';
+//     myList.appendChild(newListItem);
+//   }
+
+
+//TASK : 5 
+
+
+// function changeDivColor() {
+//     let element = document.getElementById("myElement");
+//     let color = "blue";
+//     element.style.backgroundColor = color;
+//   }
+  
+
+
+//TASK : 6 
+
+
+// function objectValue() {
+//     let key = "Key";
+//     let object = { 
+//       name: "Osama", 
+//       age: 19 
+//     };
+//     localStorage.setItem(key, JSON.stringify(object));
+//   }
+  
+
+//TASK : 7 
+
+// function getObject() {
+//     let key = "Key";
+//     let object = JSON.parse(localStorage.getItem(key));
+//     return object;
+//   }
+  
+
+
+//TASK : 8
+
+
+// function save() {
+//     let myObject = { 
+//       name: "Osama", 
+//       age: 19,
+//       city: "Karachi"
+//     };
+    
+
+//     for (let key in myObject) {
+//       localStorage.setItem(key, myObject[key]);
+//     }
+    
+
+//     let retrievedObject = {};
+//     for (let i = 0; i < localStorage.length; i++) {
+//       let key = localStorage.key(i);
+//       retrievedObject[key] = localStorage.getItem(key);
+//     }
+//     return retrievedObject;
+//   }
+  
