a06b6a9f44152d4525cff2428f5cb5c0c5ddd17c
Osama-14 <mohdosama42000@gmail.com>
2023-04-11 12:59:30 +0500

    completing tasks

---
+++ app.js
@@ -0,0 +1,54 @@
+//tASK : 1
+
+// function addingNumber(number) {
+//     return function(a) {
+//       return a + number;
+//     }
+//   }
+//  const adding = addingNumber(10);
+//  console.log(adding(30));
+//  console.log(adding(40));
+
+
+
+
+//TASK : 2
+
+// const countings = [5, 6, 7, 8, 9, 10];
+
+// function SearchingNumbers(arr, value) {
+//   if (arr.length === 0) {
+//     return false;
+//   } else if (arr[0] === value) {
+//     return true;
+//   } else {
+//     return SearchingNumbers(arr.slice(1), value);
+//   }
+// }
+
+// console.log(SearchingNumbers(countings, 2));
+// console.log(SearchingNumbers(countings, 6));
+
+
+
+//TASK : 3
+
+//    function addingPara() {
+//         const textsDiv = document.getElementById('texts');
+//         const newParagraph = document.createElement('p');
+//         newParagraph.textContent = 'This is a new paragraph.';
+//         textsDiv.appendChild(newParagraph);
+//       }
+
+
+
+
+//TASK : 4
+
+
+// function addListItem() {
+//     const myList = document.getElementById('myList');
+//     const newListItem = document.createElement('li');
+//     newListItem.textContent = 'This is a new list item.';
+//     myList.appendChild(newListItem);
+//   }
